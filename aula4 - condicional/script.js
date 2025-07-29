471e7eaf5a15fe92be084ca0c86686155c2649d2
eduabreu83 <eduardoabreu83@gmail.com>
2025-07-29 19:38:56 -0300

    at

--- aula4 - condicional/script.js
+++ aula4 - condicional/script.js
@@ -1,13 +1,21 @@
-let idade = 41;
-
-if (idade >= 42) {
+let idade = 46;
+let idade2 = "41"
+;
+if (idade >= 18 && idade < 46) {
     console.log("vc esta velho");
 } else {
     console.log("vc ainda não é velho")
 }
 
-if (idade == 41) {
-    console.log("vc ainda náo e velho");
+
+if (idade >= 18 || idade <= 46) {
+    console.log("vc esta velho");
 } else {
     console.log("vc ainda não é velho")
+}
+
+if (idade2 === 47) {
+    console.log("vc ainda náo e velho");
+} else {
+    console.log("vc é velho")
 }
