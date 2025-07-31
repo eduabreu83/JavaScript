8577fd87199c9a4947d5ca277d9068cdbb8ceea0
eduabreu83 <eduardoabreu83@gmail.com>
2025-07-31 10:25:03 -0300

    at

--- aula4 - condicional/script.js
+++ aula4 - condicional/script.js
@@ -19,3 +19,42 @@ if (idade2 === 47) {
 } else {
     console.log("vc é velho")
 }
+
+
+let idade3 = 18
+
+if(idade3 < 18){
+    console.log("vc é uma criança")
+} else if (idade3 >= 18 && idade <60) {
+    console.log("vc é adulto")
+} else if(idade3 >=60){
+    console.log("vc é velho")
+}
+
+//condicional ternario
+
+let isMember = true;
+
+let shippeing = isMember ? 2 : 10;
+
+
+console.log("frete é " + shippeing)
+
+//condiçao de switch
+
+let profissao = "fiscal";
+
+switch(profissao) {
+    case 'fical':
+        console.log('sua camisa vai ser VERDE');
+        break;
+    case 'bombeiro':
+        console.log('sua camisa vai ser AMARELO');
+        break
+    case 'policial':
+        console.log('sua camisa vai ser AZUL');
+        break
+    default:
+        console.log('sua camisa vai ser PRETA');
+        break
+}
---
+++ aula5 - funcao/script.js
@@ -0,0 +1,43 @@
+function gravidade () {
+    let gravi = 10
+    console.log(`A gravidade do planeta é de ${gravi}`);
+}
+
+gravidade();
+
+// funcoes com parametros
+function somar(n1, n2){
+    let resultado = n1 + n2
+    console.log('Resultado ' + resultado)
+}
+somar(4,4)
+
+function nomeCompleto(nome, sobrenome) {
+    console.log(`${nome} ${sobrenome}`)
+}
+
+nomeCompleto("Eduardo", "Abreu")
+
+function nomeCompleto2(nome, sobrenome) {
+    return`${nome} ${sobrenome}`
+}
+console.log(nomeCompleto2("livia", "Abreu"));
+
+// ENTRADA -> PROCESSAMENTO -> SAIDA
+
+function maiorIdade(idade) {
+    if (idade >=18){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let idade = 20;
+let verificacao = maiorIdade(idade);
+
+if (verificacao) {
+    console.log('é maior de idade');
+} else{
+    console.log('é menor de idade')
+}
