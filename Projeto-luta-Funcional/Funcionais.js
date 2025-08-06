e4e59eb825e36ac1a78715fbf1348b0cf98fc281
eduabreu83 <eduardoabreu83@gmail.com>
2025-08-06 10:40:52 -0300

    at

--- Projeto-luta-Funcional/Funcionais.js
+++ Projeto-luta-Funcional/Funcionais.js
@@ -62,25 +62,63 @@ const stage = {
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
 
-        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2) );
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
 
-        
-        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1) );
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
 
         this.update();
     },
     
     update(){
-        //Fighter1
-        this.fighter1.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.tofixed(1)} HP`;
 
-        //Fighter2
-        this.fighter2.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.tofixed(1)} HP` 
+    //Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`
 
+        //Fighter 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`
     },
 
-    doAttack(attacking, attacked){
+        doAttack(attacking, attacked){
+        if(attacking.life <= 0 || attacked.life <=0){
+            log.addMessage('atacando um morto')
+            return
+        }
+        // Gerando fator de ataque e de defesa a cada clique do botao
+        const attackFactor = (Math.random() * 2).toFixed(2)
+        const defenseFactor = (Math.random() * 2).toFixed(2)
 
+        const actualAttack = attacking.attack * attackFactor;
+        const actualDefense = attacked.defense * defenseFactor;
+
+        if (actualAttack > actualDefense){
+            attacked.life -= actualAttack
+            attacked.life = attacked.life <0 ? 0: attacked.life
+          log.addMessage(`${attacking.name} causou ${actualAttack} de dano`)
+        } else{
+            log.addMessage(`${attacked.name} Conseguiu defender...`)
+        }
+
+        this.update();
     }
 
+}
+
+const log = {
+    list: [],
+    addMessage(msg){
+       this.list.push(msg)
+       this.render() 
+    },
+    render(){
+        const logEl = document.querySelector('.log')
+        logEl.innerHTML = ''
+
+        for(let i in this.list){
+            logEl.innerHTML += `<li>${this.list[i]}</li>`
+        }
+    }
 }
