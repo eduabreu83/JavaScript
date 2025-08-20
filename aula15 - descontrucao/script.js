0effb786d27512317b45fbabf71e2df8e79ef866
eduabreu83 <eduardoabreu83@gmail.com>
2025-08-20 16:58:57 -0300

    at

--- aula15 - descontrucao/script.js
+++ aula15 - descontrucao/script.js
@@ -3,7 +3,10 @@ let pessoa = {
     sobrenome:'Abreu',
     idade: 42,
     social:{
-        insta: 'educenora',
+        insta:{
+            url:'eduabreu83',
+            seguifores: '1000'
+        },
         X: 'Cenoralost'
     },
     nomeCompleto: function() {
@@ -11,4 +14,33 @@ let pessoa = {
     }
 }
 
-console.log(pessoa.nomeCompleto())
+// para nao pegar cada dado do objeto criando variavei separadas podemos descontrui-lo:
+
+let {nome, sobrenome,social:{insta:{url:insta}}} = pessoa
+console.log(nome, sobrenome, insta);
+
+
+function pegarNomeCompleto({nome, sobrenome}) {
+    return `${nome} ${sobrenome}`
+}
+
+console.log(pegarNomeCompleto(pessoa))
+
+
+// Descontruir Arrays
+
+let info = ['eduardo Abreu', 'Eduardo', 'Abreu', '@eduabreu'];
+
+let [nomeCompleto1, , , social1] = info;
+// para nao precisar pegar todos é só colocar a virgula 
+console.log(nomeCompleto1,social1)
+
+//descontruçao de funçao / objeto
+function criar() {
+    return [1,2,3]
+}
+
+let [a,b,c] = criar()
+
+console.log(a,b,c)  
+
