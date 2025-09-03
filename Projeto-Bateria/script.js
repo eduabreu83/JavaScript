a4982a2069f74a4619b3d7e818070150e98b79fa
eduabreu83 <eduardoabreu83@gmail.com>
2025-09-03 17:24:16 -0300

    at

---
+++ Projeto-Bateria/script.js
@@ -0,0 +1,46 @@
+// monitora a teclagem 
+
+document.body.addEventListener('keyup',(event) =>{
+    playSound(event.code.toLocaleLowerCase())
+})
+
+//monitora o que foi digitado e excuta a funçao para tocar a musica
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+
+    if (song !== ''){
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+})
+
+// funçao que 
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+    if (audioElement) {
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
+
+    if(keyElement){
+        keyElement.classList.add('active');
+
+        setTimeout(()=>{
+            keyElement.classList.remove('active')
+        },300)
+    }
+}
+
+function playComposition(songArray) {
+    let wait = 0;
+
+    for (let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`)
+        }, wait)
+
+        wait += 250
+    }
+}
