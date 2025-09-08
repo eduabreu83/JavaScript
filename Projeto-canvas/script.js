6d688abb931fe00829ccea5d96f4af65e6cfa0cf
eduabreu83 <eduardoabreu83@gmail.com>
2025-09-08 14:45:27 -0300

    at

---
+++ Projeto-canvas/script.js
@@ -0,0 +1,21 @@
+//initial data
+
+let currentColor = 'black'
+
+let screen = document.querySelector('#tela');
+
+let ctx = screen.getContext('2d')
+
+//events
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent)
+})
+//functions
+function colorClickEvent(e){
+    let color = e.target.getAttribute('data-color')
+    currentColor = color
+
+    document.querySelector('.color.active').classList.remove('active')
+
+    e.target.classList.add('active')
+}
