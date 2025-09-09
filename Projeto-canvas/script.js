08248a7bf60ce84a6756f796455ed14c727ee03f
eduabreu83 <eduardoabreu83@gmail.com>
2025-09-09 15:23:38 -0300

    at

--- Projeto-canvas/script.js
+++ Projeto-canvas/script.js
@@ -1,6 +1,9 @@
 //initial data
 
 let currentColor = 'black'
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector('#tela');
 
@@ -13,28 +16,54 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
 
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
-screen.addEventListener('mouseUp', mouseUpEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+document.querySelector('.clear').addEventListener('click', clearScreen);
 
 
 
 //functions
 function colorClickEvent(e){
     let color = e.target.getAttribute('data-color')
-    currentColor = color
-
-    document.querySelector('.color.active').classList.remove('active')
-
-    e.target.classList.add('active')
+    currentColor = color;
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
 }
 
-function mouseDownEvent(){
-
+function mouseDownEvent(e){
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
 }
 
-function mouseMoveEvent(){
-
+function mouseMoveEvent(e){
+    if (canDraw){
+        draw(e.pageX, e.pageY)
+    }
 }
 
 function mouseUpEvent(){
-    
+    canDraw = false
+}
+
+function draw(x, y){
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen(){
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 }
