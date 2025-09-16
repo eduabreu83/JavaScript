d0f9ad10efbff8aae47e1b1162b073baa046c822
eduabreu83 <eduardoabreu83@gmail.com>
2025-09-16 10:31:26 -0300

    at

--- Projeto-Arrasta e solta/script.js
+++ Projeto-Arrasta e solta/script.js
@@ -18,3 +18,17 @@ function dragStart(e){
 function dragEnd(e){
     e.currentTarget.classlist.remove('dragging')
 }
+
+function dragOver(e){
+    e.preventDefault();
+    e.currentTarget.classlist.add('hover')
+}
+
+function dragLeave(e){
+    e.currentTarget.classlist.remove('hover')
+
+}
+
+function drop(e){
+    e.currentTarget.classlist.remove('hover')
+}
